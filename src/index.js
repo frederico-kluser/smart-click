ad244b30dbb6956feb4b823140aeef21827ed420
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-04-09 04:38:42 -0300

    add icons support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
+initializeIcons();
 
 ReactDOM.render(
   <React.StrictMode>
