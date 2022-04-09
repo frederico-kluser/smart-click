9dabe129790c92aae1b79bd8c4758207bd50038a
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-04-09 04:39:03 -0300

    connect with the server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import { Spinner, SpinnerSize, Text } from '@fluentui/react';
+import { useState } from 'react';
 import { Body, Loader } from './App.styled';
+import MacroItem from './components/macroItem';
+import Macros from './pages/Macros';
 
 function App() {
+  const [loaded, setLoaded] = useState(true);
+
+  fetch('http://localhost:8080/macro/allMacros')
+    .then(response => {
+      setLoaded(false);
+      // response.json()
+    })
+    .then(data => console.log(data))
+
   return (
     <Body>
-      <Text variant="xxLarge">
+      <Text variant="xxLarge" style={{ marginBottom: 50 }}>
         Smart-Click
-        <Loader>
-          <Spinner size={SpinnerSize.large} label="Loading Macros..." />
-        </Loader>
       </Text>
+      { loaded && 
+      <Loader>
+        <Spinner size={SpinnerSize.large} label="Loading Macros..." />
+      </Loader>}
+      { !loaded && <Macros />
+      }
     </Body>
   );
 }
