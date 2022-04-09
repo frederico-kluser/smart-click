4f9b393a2b1b58e2aab8721fac04e0be93c85a7a
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-04-09 01:59:33 -0300

    remove garbage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import logo from './logo.svg';
 import { Link, Text } from '@fluentui/react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Text>
-        When a link has an href,{' '}
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/controls/web/link" underline>
-          it renders as an anchor tag.
-        </Link>{' '}
-        Without an href,{' '}
-        <Link underline>
-          the link is rendered as a button
-        </Link>
-        . You can also use the disabled attribute to create a{' '}
-        <Link disabled href="https://developer.microsoft.com/en-us/fluentui#/controls/web/link" underline>
-          disabled link.
-        </Link>
-      </Text>
-      <Text>
-        It's not recommended to use Links with imgs because Links are meant to render textual inline content. Buttons
-        are inline-block or in the case of imgs, block. However, it is possible to create a linked image button by
-        making a button with an unstyled variant and adding the img content and href source to that.
-      </Text>
+        <Text variant="mega">
+          Smart-Click
+        </Text>
       </header>
     </div>
   );
