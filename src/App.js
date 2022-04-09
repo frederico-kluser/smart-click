d1eb6af18f687ead5130c5b6540aca82b9641eb0
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-04-09 11:46:42 -0300

    add auto update system

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import { Spinner, SpinnerSize, Text } from '@fluentui/react';
 import { useState } from 'react';
 import { Body, Loader } from './App.styled';
-import MacroItem from './components/macroItem';
 import Macros from './pages/Macros';
+import { compareObject } from './utils/general';
+
+async function checkUpdate(macros, setMacros, setLoaded) {
+  await new Promise((resolve) => fetch('http://localhost:8080/macro/allMacros')
+  .then(response => response.json())
+  .then(({ data }) => {
+    setMacros(data);
+    setLoaded(true);
+    resolve();
+  }))
+  
+  while (true) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    await new Promise((resolve) => fetch('http://localhost:8080/macro/allMacros')
+    .then(response => response.json())
+    .then(({ data }) => {
+      if (!compareObject(macros, data)) {
+        setMacros(data);
+      }
+      resolve();
+    }))
+  }
+}
 
 function App() {
-  const [loaded, setLoaded] = useState(true);
-
-  fetch('http://localhost:8080/macro/allMacros')
-    .then(response => {
-      setLoaded(false);
-      // response.json()
-    })
-    .then(data => console.log(data))
+  const [loaded, setLoaded] = useState(false);
+  const [macros, setMacros] = useState([]);
+  checkUpdate(macros, setMacros, setLoaded);
 
   return (
     <Body>
-      <Text variant="xxLarge" style={{ marginBottom: 50 }}>
+      <Text variant="xxLarge">
         Smart-Click
       </Text>
-      { loaded && 
+      <Text style={{ marginBottom: 50 }}>
+        client offline
+      </Text>
+      { !loaded && 
       <Loader>
         <Spinner size={SpinnerSize.large} label="Loading Macros..." />
       </Loader>}
-      { !loaded && <Macros />
-      }
+      { loaded && <Macros data={macros} /> }
     </Body>
   );
 }
