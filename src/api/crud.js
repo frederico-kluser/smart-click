cac5060950060ca8d27930f3ec03e1ad1c17a04a
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-04-11 19:52:56 -0300

    create delete function

--- src/api/crud.js
+++ src/api/crud.js
@@ -5,3 +5,17 @@ fetch(url)
     resolve(data);
   }),
 );
+
+export const del = (url, id) => new Promise((resolve) =>
+fetch(`http://localhost:8080/macro/delete/?id=${id}`, {
+  method: 'DELETE',
+  mode: 'cors',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+  .then((response) => response.json())
+  .then(({ data }) => {
+    resolve(data);
+  }),
+);
--- src/api/urls.js
+++ src/api/urls.js
@@ -1,1 +1,2 @@
 export const AllMacros = 'http://localhost:8080/macro/allMacros';
+export const deleteMacroURL = 'http://localhost:8080/macro/delete/?id=';
