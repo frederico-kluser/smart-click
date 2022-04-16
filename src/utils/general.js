ac60259c3f5eefd30d0b333c370a549718dcc0f6
Frederico Guilherme Kluser de Oliveira <frederico.kluser@onepeloton.com>
2022-04-16 09:10:34 -0300

    create helper functions

--- src/utils/general.js
+++ src/utils/general.js
@@ -1,1 +1,11 @@
 export const compareObject = (arr1, arr2) => JSON.stringify(arr1) === JSON.stringify(arr2);
+
+export const getStringBetween = (str, start, end) => {
+  const startIndex = str.indexOf(start);
+  const endIndex = str.indexOf(end || start, startIndex + 1);
+  return str.substring(startIndex + start.length, endIndex);
+};
+
+export const stringReplaceAll = (str, find, replace) => {
+  return str.replace(new RegExp(find, 'g'), replace);
+};
