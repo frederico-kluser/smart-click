4170f750fe94d623c105798d7f65dbbb9fec69ac
Frederico Guilherme Kluser de Oliveira <frederico.kluser@onepeloton.com>
2022-04-16 10:38:55 -0300

    create window.prompt for electron

---
+++ src/utils/nativeFunctions.js
@@ -0,0 +1,19 @@
+import { modalPrompt } from "../components/Modal";
+import eventEmitter from "./event";
+import { getStringBetween, stringReplaceAll } from "./general";
+import { variableNameGetter, xmlGetter } from "./globalVariables";
+
+export const windowPrompt = async () => {
+  const xml = xmlGetter();
+  const promptText = variableNameGetter();//"Rename all 'item' variables to:";
+
+  const variablePreviousName = getStringBetween(promptText, "'");
+  const variableNextName = await modalPrompt({
+    tile: 'Rename Variable',
+    subTitle: `Rename the variable '${variablePreviousName}' to:`,
+    placeholder: "New variable name",
+  });
+
+  const newXML = stringReplaceAll(xml, variablePreviousName, variableNextName); 
+  eventEmitter.emit('changeXML', newXML);
+};
